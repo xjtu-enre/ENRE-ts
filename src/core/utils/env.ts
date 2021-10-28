04d1b3d3b5351e00ae08cc98ae51388cf51876c4
ThisRabbit <1195398892@qq.com>
2021-10-28 22:29:07 +0800

    fix: node_env implicitly assign warn message cannot be shown due to cliRender was not yet initialized

--- src/core/utils/env.ts
+++ src/core/utils/env.ts
@@ -1,4 +1,4 @@
-import {warn} from './cliRender';
+import chalk from 'chalk';
 
 export interface env {
   dev: boolean,
@@ -7,7 +7,9 @@ export interface env {
 }
 
 if (process.env.NODE_ENV === undefined) {
-  warn('Environment variable NODE_ENV is not assigned, PRODUCTION is implicitly used');
+  // This piece of code is run before cliRender is initialized, so direct chalk invoke is used
+  console.warn(chalk.black.bgYellowBright(' WARN ') + ' ' +
+    'Environment variable NODE_ENV is not assigned, PRODUCTION is implicitly used');
   process.env.NODE_ENV = 'production';
 }
 
