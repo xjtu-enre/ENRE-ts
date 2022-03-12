1b15be4ac19f0adec8c4bf45babbe15ad830bb26
ThisRabbit <1195398892@qq.com>
2022-03-12 22:34:33 +0800

    chore: update schema

--- scripts/md2/metaSchema.mjs
+++ scripts/md2/metaSchema.mjs
@@ -2,36 +2,48 @@
  * Defines schema for meta describing testcase group.
  */
 export const groupSchema = {
+  type: 'object',
   properties: {
     /**
      * Group's name, using for generating case directory.
      */
     name: {type: 'string'},
-  },
-  optionalProperties: {
     /**
-     * Indicates the code snippet should be treated as ESM if true.
+     * Indicates the code snippet should be treated as ESM if true, else .
      * @default false
      */
     module: {type: 'boolean'},
   },
+  required: ['name'],
+  additionalProperties: false,
 };
 
 /**
  * Defines schema for meta describing single testsuite.
  */
 export const caseSchema = {
+  type: 'object',
   properties: {
     /**
      * Case's name
      */
     name: {type: 'string'},
-  },
-  optionalProperties: {
     /**
      * Will be inherited from belonging group if not set.
      */
     module: {type: 'boolean'},
-    filter: {type: ['array', 'string']}
+    /**
+     * Filter cared entity type.
+     * Will fetch all entity if not set.
+     */
+    filter: {
+      oneOf: [
+        {type: 'string'},
+        {type: 'array', uniqueItems: true, items: {type: 'string'}}
+      ]
+    },
+    entities: {}
   },
+  required: true,
+  additionalProperties: false,
 };
