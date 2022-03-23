4d049ad2e95b325f85d7df4de4392a320cdd8bcf
ThisRabbit <thisrabbit@qq.com>
2022-03-23 23:12:13 +0800

    feat: add handler for `CatchClause` to extract `Entity: Parameter`

---
+++ src/core/analyser/traverseOpts/CatchClause.ts
@@ -0,0 +1,45 @@
+/**
+ * CatchClause
+ *
+ * Extractable entity:
+ *   * Parameter
+ */
+
+import {ENREEntityCollectionScoping, ENRELocation} from '../entities';
+import {NodePath} from '@babel/traverse';
+import {CatchClause} from '@babel/types';
+import {verbose} from '../../utils/cliRender';
+import {buildENRECodeName, ENRENameBuildOption} from '../../utils/nameHelper';
+import {ENREEntityParameter, recordEntityParameter} from '../entities/eParameter';
+import handleBindingPatternRecursively from './common/handleBindingPatternRecursively';
+
+const onRecord = (name: string, location: ENRELocation, scope: Array<ENREEntityCollectionScoping>) => {
+  return recordEntityParameter(
+    buildENRECodeName(ENRENameBuildOption.value, name),
+    location,
+    scope[scope.length - 1],
+  );
+};
+
+const onLog = (entity: ENREEntityParameter) => {
+  verbose('Record Entity Parameter (catch): ' + entity.name);
+};
+
+export default (scope: Array<ENREEntityCollectionScoping>) => {
+  return {
+    enter: (path: NodePath<CatchClause>) => {
+      if (path.node.param) {
+        handleBindingPatternRecursively<ENREEntityParameter>(
+          path.node.param,
+          scope,
+          onRecord,
+          onLog,
+        );
+      }
+    },
+
+    exit: () => {
+      // scope.pop();
+    }
+  };
+};
--- src/core/analyser/traverseOpts/index.ts
+++ src/core/analyser/traverseOpts/index.ts
@@ -2,11 +2,13 @@ import VariableDeclaration from './VariableDeclaration';
 import {ENREEntityCollectionScoping} from '../entities';
 import FunctionDeclaration from './FunctionDeclaration';
 import ArrowFunctionExpression from './ArrowFunctionExpression';
+import CatchClause from './CatchClause';
 
 export default (scopeProvider: Array<ENREEntityCollectionScoping>) => {
   return {
     'VariableDeclaration': VariableDeclaration(scopeProvider),
     'FunctionDeclaration|FunctionExpression': FunctionDeclaration(scopeProvider),
     'ArrowFunctionExpression': ArrowFunctionExpression(scopeProvider),
+    'CatchClause': CatchClause(scopeProvider),
   };
 };
