5e6946e2c71d6e50701eb9b7d79c06cda85520f0
ThisRabbit <thisrabbit@qq.com>
2022-03-23 16:03:40 +0800

    chore: using `as` rather than `!` to discard `null`

--- src/core/analyser/traverseOpts/VariableDeclaration.ts
+++ src/core/analyser/traverseOpts/VariableDeclaration.ts
@@ -6,7 +6,7 @@
  */
 
 import {NodePath} from '@babel/traverse';
-import {Identifier, PatternLike, RestElement, VariableDeclaration} from '@babel/types';
+import {Identifier, PatternLike, RestElement, SourceLocation, VariableDeclaration} from '@babel/types';
 import {verbose} from '../../utils/cliRender';
 import {ENREEntityCollectionScoping, ENRELocation} from '../entities';
 import {ENREEntityVariable, ENREEntityVariableKind, recordEntityVariable} from '../entities/eVariable';
@@ -30,72 +30,72 @@ const handleBindingPatternRecursively = (
   let entity;
 
   switch (id.type) {
-  case 'Identifier':
-    entity = buildHelper(
-      id.name,
-      toENRELocation(id.loc!)
-    );
-    verbose('VariableDeclaration: ' + entity.name);
-    break;
+    case 'Identifier':
+      entity = buildHelper(
+        id.name,
+        toENRELocation(id.loc as SourceLocation)
+      );
+      verbose('VariableDeclaration: ' + entity.name);
+      break;
 
-  case 'RestElement':
-    entity = buildHelper(
-      (id.argument as Identifier).name,
-      toENRELocation(id.argument.loc!)
-    );
-    verbose('VariableDeclaration: ' + entity.name);
-    break;
+    case 'RestElement':
+      entity = buildHelper(
+        (id.argument as Identifier).name,
+        toENRELocation(id.argument.loc as SourceLocation)
+      );
+      verbose('VariableDeclaration: ' + entity.name);
+      break;
 
-  case 'AssignmentPattern':
-    handleBindingPatternRecursively(
-      id.left as PatternLike,
-      scope,
-      kind
-    );
-    break;
+    case 'AssignmentPattern':
+      handleBindingPatternRecursively(
+        id.left as PatternLike,
+        scope,
+        kind
+      );
+      break;
 
-  case 'ObjectPattern':
-    for (const property of id.properties) {
-      if (property.type === 'RestElement') {
-        // It's argument can only be Identifier
-        handleBindingPatternRecursively(
-          property.argument as RestElement,
-          scope,
-          kind
-        );
-      } else {
-        // property.type === 'ObjectProperty'
-        handleBindingPatternRecursively(
-          property.value as PatternLike,
-          scope,
-          kind
-        );
+    case 'ObjectPattern':
+      for (const property of id.properties) {
+        if (property.type === 'RestElement') {
+          // It's argument can only be Identifier
+          handleBindingPatternRecursively(
+            property.argument as RestElement,
+            scope,
+            kind
+          );
+        } else {
+          // property.type === 'ObjectProperty'
+          handleBindingPatternRecursively(
+            property.value as PatternLike,
+            scope,
+            kind
+          );
+        }
       }
-    }
-    break;
+      break;
 
-  case 'ArrayPattern':
-    for (const element of id.elements) {
-      if (element === null) {
-        continue;
-      }
+    case 'ArrayPattern':
+      for (const element of id.elements) {
+        if (element === null) {
+          continue;
+        }
 
-      if (element.type === 'RestElement') {
-        handleBindingPatternRecursively(
-          element.argument as RestElement,
-          scope,
-          kind
-        );
-      } else {
-        // element.type === 'PatternLike'
-        handleBindingPatternRecursively(
-          element as PatternLike,
-          scope,
-          kind
-        );
+        if (element.type === 'RestElement') {
+          handleBindingPatternRecursively(
+            element.argument as RestElement,
+            scope,
+            kind
+          );
+        } else {
+          // element.type === 'PatternLike'
+          handleBindingPatternRecursively(
+            element as PatternLike,
+            scope,
+            kind
+          );
+        }
       }
-    }
-    break;
+      break;
   }
 };
 
