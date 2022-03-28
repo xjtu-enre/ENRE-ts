4e08c6b19f90914960f69b99f8370ec7f1534167
ThisRabbit <thisrabbit@qq.com>
2022-03-28 14:22:20 +0800

    chore: remove TODO in comments and use issues to manage TODOs

--- src/core/analyser/entities/eFunction.ts
+++ src/core/analyser/entities/eFunction.ts
@@ -44,13 +44,3 @@ export const recordEntityFunction = (
 
   return _obj;
 };
-
-/**
- * TODO: Whether to separate a variable initialized by a function as 2 entities is still discussable
- * Refactor the part of function expression
- */
-
-/**
- * TODO: Function constructor
- * see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/Function
- */
