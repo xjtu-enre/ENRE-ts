67a91b41c46439f15dc37adfe3cac5fc944493eb
ThisRabbit <thisrabbit@qq.com>
2022-07-23 12:48:35 +0800

    chore: update testcase name conversion to preserve dash(-)

--- packages/enre-doc-meta-parser/src/common.ts
+++ packages/enre-doc-meta-parser/src/common.ts
@@ -3,5 +3,5 @@ import Ajv from 'ajv';
 export const ajv = new Ajv({useDefaults: true, allowUnionTypes: true});
 
 export const toUrlFriendlyName = (raw: string) => {
-  return raw.replaceAll(/[^a-zA-Z\d]+/g, '-').toLowerCase();
+  return raw.replaceAll(/[^a-zA-Z\d-]+/g, '-').toLowerCase();
 };
