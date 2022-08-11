155f4e06a976e5a5c9a4a42f6ee63b1c0a9d0f2a
ThisRabbit <thisrabbit@qq.com>
2022-08-11 20:22:04 +0800

    feat(doc-meta-parser): add group meta rule `freeForm` to skip strict format validation

--- packages/enre-doc-meta-parser/src/group-meta/raw.ts
+++ packages/enre-doc-meta-parser/src/group-meta/raw.ts
@@ -12,6 +12,13 @@ export const schemaObj: JSONSchemaType<GroupSchema> = {
      * It is preferred to use space, space will be transcribed to `-`.
      */
     name: {type: 'string'},
+    /**
+     * Skip strict format check.
+     * This is useful for cases that are not convenient or too complicated to write into docs.
+     *
+     * However, it is suggested that still writes tests fixtures and test suites directly under /tests.
+     */
+    freeForm: {type: 'boolean', default: false},
   },
   required: ['name'],
   additionalProperties: false,
@@ -19,4 +26,5 @@ export const schemaObj: JSONSchemaType<GroupSchema> = {
 
 export interface GroupSchema {
   name: string;
+  freeForm: boolean;
 }
