f01928473eda341d56bbf5b15edea6b1296e7da5
ThisRabbit <thisrabbit@qq.com>
2022-08-24 13:36:15 +0800

    chore(cross-tester): reuse previous json output to reduce execution time

--- packages/enre-cross-tester/src/adapters/enre/python/index.ts
+++ packages/enre-cross-tester/src/adapters/enre/python/index.ts
@@ -4,18 +4,26 @@ import builder from './builder';
 import {error} from '@enre/logging';
 import {CaseContainer} from '@enre/doc-parser';
 import {PYMatcher} from '../../../matchers';
+import {readFile} from 'node:fs/promises';
 
 export default async (g: string, c: string, cs: CaseContainer, ocwd: string, exepath: string) => {
-  if (await creator(g, c, exepath)) {
-    const data = await extractor(g, c, ocwd);
-    if (data) {
-      console.log(data.replaceAll(/\s+/g, ' '));
-      builder(data);
-      return PYMatcher(cs);
+  try {
+    const data = await readFile(`${process.cwd()}/tests/enre/${g}/${c}/_${c}-report-enre.json`, 'utf-8');
+    console.log(data.replaceAll(/\s+/g, ' '));
+    builder(data);
+    return PYMatcher(cs);
+  } catch {
+    if (await creator(g, c, exepath)) {
+      const data = await extractor(g, c, ocwd);
+      if (data) {
+        console.log(data.replaceAll(/\s+/g, ' '));
+        builder(data);
+        return PYMatcher(cs);
+      } else {
+        error(`Failed to read enre output on ${g}/${c}`);
+      }
     } else {
-      error(`Failed to read enre output on ${g}/${c}`);
+      error(`Failed to execute enre on ${g}/${c}`);
     }
-  } else {
-    error(`Failed to execute enre on ${g}/${c}`);
   }
 };
