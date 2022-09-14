ae259d05a8fa34060de48d96ee94b77dbb4ba7a1
thisrabbit <thisrabbit@qq.com>
2022-09-14 14:01:04 +0800

    feat(naming): add ENRENameFile type and implementation

---
+++ packages/enre-naming/src/xml/allowed-tags.ts
@@ -0,0 +1,5 @@
+export default [
+  'File',
+  'Anonymous',
+  'Modified',
+] as const;
---
+++ packages/enre-naming/src/xml/tag-anonymous.ts
@@ -0,0 +1,23 @@
+/**
+ * Allowed types for `Anonymous` entity name.
+ */
+export const ENRENameAnonymousTypes = [
+  'Function',
+  'ArrowFunction',
+  'Class',
+  'CallableSignature',
+  'NumberIndexSignature',
+  'StringIndexSignature'
+] as const;
+
+export type ENRENameAnonymousType = typeof ENRENameAnonymousTypes[number];
+
+/**
+ * An object representing properties needed by an anonymous entity.
+ */
+export type ENRENameAnonymous = {
+  /**
+   * Indicates in which type of anonymous entity this is.
+   */
+  readonly as: ENRENameAnonymousType;
+}
