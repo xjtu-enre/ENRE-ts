355495c1d9487f7dbf08ee9b59826869ad90db59
thisrabbit <thisrabbit@qq.com>
2023-09-25 14:56:17 +0800

    fix: test fail due to updated `usingCore` accepts an array of input paths

--- packages/enre-test-generator/src/templates/singleCase.ts
+++ packages/enre-test-generator/src/templates/singleCase.ts
@@ -2,7 +2,7 @@ import template from '@babel/template';
 
 export default template.smart(`describe(%%name%%, () => {
   beforeAll(async () => {
-      await usingCore(%%path%%);
+      await usingCore([%%path%%]);
     });
     afterAll(() => {
       cleanAnalysis();
